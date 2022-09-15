841509bb51f53827c5cdabd4ac2a2e23f03f69c0
iLoveVases <73852711+iLoveVases@users.noreply.github.com>
2022-09-15 22:50:10 +0200

    Starting GUI_dev branch
    development of GUI Feature

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import CreateDataBase.Renamer as Renamer
-from CreateDataBase import Renamer
+from tkinter import *
 
 if __name__ == '__main__':
-    Renamer.arrange()
+    root = Tk(className=r"Pose Estimation System")
+    root.geometry("350x100")
+
+    # Define buttons and labels
+    rename_label = Label(root, text="Prepare photo samples for processing")
+    rename_button = Button(root, text="Rename", command=Renamer.arrange)
+
+    # Positioning on the screen
+    rename_button.place(x=40, y=20)
+    rename_label.place(x=100, y=23)
+
+    # Plot
+    root.mainloop()
