c4028dbf42333dde1975b748243318b09fae5102
iLoveVases <73852711+iLoveVases@users.noreply.github.com>
2022-09-15 20:41:40 +0200

    Renamer implementation
    Now Renamer function can be started from main.py as a python package

--- CreateDataBase/Renamer.py
+++ CreateDataBase/Renamer.py
@@ -3,35 +3,38 @@ import glob
 import os
 from natsort import natsorted, ns
 from definitions import IMAGES_PATHS
+
 # VARIABLES ----------------------------------------------------------------------------
-PATHS_TO_RENAME = [x+r'\\' for x in IMAGES_PATHS]
+PATHS_TO_RENAME = [x + r'\\' for x in IMAGES_PATHS]
 
-# MAIN  ----------------------------------------------------------------------------
-for path in PATHS_TO_RENAME:
-    # search for .jpg files
-    pattern = path + "*.jpg"
 
-    # List of the files that match the pattern
-    result = glob.glob(pattern)
-    result = natsorted(result, alg=ns.IGNORECASE)
-    # Iterating the list with the count
-    count = 1
-    for file_name in result:
-        old_name = file_name
-        new_name = path + str(count) + "temp" + ".jpg"
-        os.rename(old_name, new_name)
-        count = count + 1
+# FUNCTIONS  ----------------------------------------------------------------------------
+def arrange():
+    for path in PATHS_TO_RENAME:
+        # search for .jpg files
+        pattern = path + "*.jpg"
 
-    result = glob.glob(pattern)
-    result = natsorted(result, alg=ns.IGNORECASE)
-    count = 1
-    for file_name in result:
-        old_name = file_name
-        new_name = path + str(count) + ".jpg"
-        os.rename(old_name, new_name)
-        count = count + 1
+        # List of the files that match the pattern
+        result = glob.glob(pattern)
+        result = natsorted(result, alg=ns.IGNORECASE)
+        # Iterating the list with the count
+        count = 1
+        for file_name in result:
+            old_name = file_name
+            new_name = path + str(count) + "temp" + ".jpg"
+            os.rename(old_name, new_name)
+            count = count + 1
 
-    # printing all revenue txt files
-    res = sorted(glob.glob(path + "*.jpg"))
-    # for name in res:
+        result = glob.glob(pattern)
+        result = natsorted(result, alg=ns.IGNORECASE)
+        count = 1
+        for file_name in result:
+            old_name = file_name
+            new_name = path + str(count) + ".jpg"
+            os.rename(old_name, new_name)
+            count = count + 1
+
+        # printing all revenue txt files
+        res = sorted(glob.glob(path + "*.jpg"))
+        # for name in res:
         # print(name)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
-
+import CreateDataBase.Renamer as Renamer
+from CreateDataBase import Renamer
 
 if __name__ == '__main__':
-    pass
+    Renamer.arrange()
