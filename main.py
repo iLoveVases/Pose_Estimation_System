1e3d29e42df49af114a844a474db0e8b134e9435
iLoveVases <73852711+iLoveVases@users.noreply.github.com>
2022-09-18 21:09:26 +0200

    GUI adding text box with label

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     root = Tk(className=r"Pose Estimation System")
     root.geometry("350x150")
 
+
     # Define buttons and labels
     rename_label = Label(root, text="Prepare photo samples for processing")
     rename_button = Button(root, text="Rename", command=Renamer.arrange)
@@ -17,7 +18,11 @@ if __name__ == '__main__':
     convert_label = Label(root, text="Generate CSV file based on numerical data")
     convert_button = Button(root, text="Convert", command=CSV_generate.write_csv)
 
+    csv_name = Entry(root, width=15)
+    csv_name_label = Label(root, text="CSV Name:")
+
     # Positioning on the screen
+
     rename_label.place(x=100, y=23)
     rename_button.place(x=40, y=20)
 
@@ -26,6 +31,9 @@ if __name__ == '__main__':
 
     convert_label.place(x=100, y=83)
     convert_button.place(x=40, y=80)
+
+    csv_name.place(x=170, y=112)
+    csv_name_label.place(x=100, y=110)
 
     # Plot
     root.mainloop()
